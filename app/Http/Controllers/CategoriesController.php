113343b9322e095188262da9da2064eb78aad332
daaangt1 <59860240+daaangt@users.noreply.github.com>
2021-09-04 23:59:12 -0300

    Categories finished

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Request;
+
+class CategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Category::All();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        try {
+            $category = $request->validated();
+
+            if ($request->hasFile('file')) {
+                $request->file('file')->move(public_path('categories\\'), $request->name . "." . $request->file('file')->getClientOriginalExtension());
+                $category['file'] = 'categories\\' . $request->name . "." . $request->file('file')->getClientOriginalExtension();
+            }
+
+            Category::create($category);
+        } catch (\Exception $e) {
+            if (env('APP_DEBUG')) {
+                throw new HttpResponseException(response()->json(['error' => $e->getMessage()], 500));
+            } else {
+                throw new HttpResponseException(response()->json(['error' => 'An error ocurred, in the store category attempt!'], 500));
+            }
+        }
+        return response()->json(['sucess' => 'Category stored correctly!'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Category::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCategoryRequest $request, $id)
+    {
+        try {
+            $category = Category::find($id);
+
+            $updatedCategory = $request->validated();
+
+            if ($request->hasFile('file')) {
+
+                if (!is_null($category->file) && file_exists(public_path($category->file))) {
+                    unlink(public_path($category->file));
+                }
+
+                $request->file('file')->move(public_path('categories\\'), $request->name . "." . $request->file('file')->getClientOriginalExtension());
+                $updatedCategory['file'] = 'categories\\' . $request->name . "." . $request->file('file')->getClientOriginalExtension();
+            }
+
+            $category->update($updatedCategory);
+        } catch (\Exception $e) {
+            if (env('APP_DEBUG')) {
+                throw new HttpResponseException(response()->json(['error' => $e->getMessage()], 500));
+            } else {
+                throw new HttpResponseException(response()->json(['error' => 'An error ocurred, in the update category attempt!'], 500));
+            }
+        }
+        return response()->json(['sucess' => 'Category updated correctly!'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $category = Category::find($id);
+
+            if (!is_null($category->file) && file_exists(public_path($category->file))) {
+                unlink(public_path($category->file));
+            }
+
+            $category->delete();
+        } catch (\Exception $e) {
+            if (env('APP_DEBUG')) {
+                throw new HttpResponseException(response()->json(['error' => $e->getMessage()], 500));
+            } else {
+                throw new HttpResponseException(response()->json(['error' => 'An error ocurred, in the delete category attempt!'], 500));
+            }
+        }
+
+        return response()->json(['sucess' => 'Category deleted correctly!'], 200);
+    }
+}
---
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:3',
+            'file' => 'required|image|mimes:jpeg,jpg,png'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors();
+
+        $response = response()->json([
+            'message' => 'Invalid data send',
+            'details' => $errors->messages(),
+        ], 422);
+
+        throw new HttpResponseException($response);
+    }
+}
